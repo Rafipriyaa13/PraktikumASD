fac6cd209688382922dd87f197480e36ec430de1
rafipriyaaa <priyanugraha1306@gmail.com>
2026-06-11 07:29:28 +0700

    P14_Percobaan2

---
+++ P14/BinaryTreeArray23.java
@@ -0,0 +1,25 @@
+package P14;
+
+public class BinaryTreeArray23 {
+    Mahasiswa23[] dataMahasiswa;
+    int idxLast;
+
+    public BinaryTreeArray23() {
+        this.dataMahasiswa = new Mahasiswa23[10];
+    }
+
+    void populateData(Mahasiswa23[] dataMhs, int idxLast) {
+        this.dataMahasiswa = dataMhs;
+        this.idxLast = idxLast;
+    }
+
+    void traverseInOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            if (dataMahasiswa[idxStart] != null) {
+                traverseInOrder(2 * idxStart + 1);
+                dataMahasiswa[idxStart].tampilInfomasi();
+                traverseInOrder(2 * idxStart + 2);
+            }
+        }
+    }
+}
