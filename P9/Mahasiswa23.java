78e9db59e845695792a7ceccbefefbf59452e297
rafipriyaaa <priyanugraha1306@gmail.com>
2026-06-26 06:30:52 +0700

    P9_Percobaan 1

---
+++ P9/Mahasiswa23.java
@@ -0,0 +1,20 @@
+package P9;
+
+public class Mahasiswa23 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa23() {
+    }
+    public Mahasiswa23(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.nilai = -1; 
+    }
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
