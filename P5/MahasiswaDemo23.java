e4200e5f53d6dde66f20c967b2f7beace2f3ad7b
rafipriyaaa <priyanugraha1306@gmail.com>
2026-06-26 03:44:42 +0700

    P5_Modifikasi 5.3.4_Percobaan 2

--- P5/MahasiswaDemo23.java
+++ P5/MahasiswaDemo23.java
@@ -1,20 +1,28 @@
+import java.util.Scanner;
+
 public class MahasiswaDemo23 {
-    public static void main(String[] args) {
-        
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi23 list = new MahasiswaBerprestasi23();
         
-        Mahasiswa23 m1 = new Mahasiswa23("123", "Rafi", "2A", 3.2);
-        Mahasiswa23 m2 = new Mahasiswa23("124", "Ayu", "2A", 3.5);
-        Mahasiswa23 m3 = new Mahasiswa23("125", "Rofi", "2A", 3.1);
-        Mahasiswa23 m4 = new Mahasiswa23("126", "Mavelynn", "2A", 3.9);
-        Mahasiswa23 m5 = new Mahasiswa23("127", "Dola", "2A", 3.7);
+        System.out.print("Masukkan jumlah mahasiswa: "); // merubah dari data hardcode ke input 
+        int jumMhs = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM: "); 
+            String nim = sc.nextLine();
+            System.out.print("Nama: "); 
+            String nama = sc.nextLine();
+            System.out.print("Kelas: "); 
+            String kelas = sc.nextLine();
+            System.out.print("IPK: "); 
+            double ipk = sc.nextDouble();
+            sc.nextLine(); 
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
-
+            Mahasiswa23 m = new Mahasiswa23(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
         System.out.println("Data mahasiswa sebelum sorting: ");
         list.tampil();
 
